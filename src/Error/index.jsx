0de164f712ff47d7b70e6b0467a6770041274299
Öjvind Otterbjörk <ojvind@otterbjork.com>
2020-11-24 23:52:22 +0100

    esLint Error component

--- src/Error/index.jsx
+++ src/Error/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './style.css';
+import PropTypes from 'prop-types';
 
 const ErrorMessage = ({ error }) => (
   <div className="ErrorMessage">
     <small>{error.toString()}</small>
   </div>
 );
+
+ErrorMessage.propTypes = {
+  error: PropTypes.string,
+};
+
+ErrorMessage.defaultProps = {
+  error: '',
+};
 
 export default ErrorMessage;
