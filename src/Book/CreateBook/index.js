dc74442945209bbb35680aa4e29c6b11bd2ae127
Öjvind Otterbjörk <ojvind@otterbjork.com>
2020-11-24 15:48:09 +0100

    eslint BookListItemDetailContainter & CreateBook

--- src/Book/CreateBook/index.js
+++ src/Book/CreateBook/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../Input';
 import Button from '../../Button';
 
@@ -10,70 +10,53 @@ import './create-book.css';
 
 import ErrorMessage from '../../Error';
 
-class CreateBook extends React.Component {
-    state = {
-        title: "",
-        yearRead: "",
-        writerId: this.props.writerId,
-        yearPublished: "",
-    };
+const CreateBook = (props) => {
+  const [title, onTitleChange] = useState('');
+  const [yearPublished, onYearPublishedChange] = useState('');
+  const [yearRead, onReadChange] = useState('');
 
-    onTitleChange = event => {
-        this.setState({ title: event.target.value });
-    };
-
-    onYearPublishedChange = event => {
-        this.setState({ yearPublished: event.target.value });
-    };
-
-    onReadChange = event => {
-        this.setState({ yearRead: event.target.value });
-    };
-
-    render() {
-        const { title, yearRead, writerId, yearPublished } = this.state;
-        return (
-        <div>
-          <div className="create-book">
-            <div className="create-book__input">
-              <Input onChange={this.onTitleChange} id="title" inputLabel="Title"/>
-            </div>
-            <div className="create-book__input">
-              <Input onChange={this.onYearPublishedChange} id="yearpublished" inputLabel="YearPublished"/>
-            </div>
-            <div className="create-book__input">
-              <Input onChange={this.onReadChange} id="read" inputLabel="Read"/>
-            </div>
-            <Mutation
-              mutation={CREATE_BOOK}
-              variables={{ title, yearRead, writerId, yearPublished }}
-              refetchQueries={[
-                { 
-                  query: GET_BOOKS,
-                  variables: { writerId } }
-                ]}
-              >
-              {(createbook, { data, loading, error }) => {
-                const button = (
-                  <Button
-                      className={'create-book__button'}
-                      onClick={createbook}
-                      color={'black'}>
-                    Create Bok och lovla K
-                  </Button>
-                );
-                if (error) {
-                  return <div><ErrorMessage error={error} />{ button }</div>;
-                }
-                return <div className="create-book__button">
-                  {button}
-                </div>; 
-              }}
-            </Mutation>
-          </div>
-        </div>
-      );
-   }
+  const writerId = props.writerId;
+  return (
+  <div>
+    <div className="create-book">
+      <div className="create-book__input">
+        <Input onChange={(e) => onTitleChange(e.target.value)} id="title" inputLabel="Title"/>
+      </div>
+      <div className="create-book__input">
+        <Input onChange={(e) => onYearPublishedChange(e.target.value)} id="yearpublished" inputLabel="YearPublished"/>
+      </div>
+      <div className="create-book__input">
+        <Input onChange={(e) => onReadChange(e.target.value)} id="read" inputLabel="Read"/>
+      </div>
+      <Mutation
+        mutation={CREATE_BOOK}
+        variables={{ title, yearRead, writerId, yearPublished }}
+        refetchQueries={[
+          { 
+            query: GET_BOOKS,
+            variables: { writerId } }
+          ]}
+        >
+        {(createbook, { data, loading, error }) => {
+          const button = (
+            <Button
+                className={'create-book__button'}
+                onClick={createbook}
+                color={'black'}>
+              Create Bok och lovla K
+            </Button>
+          );
+          if (error) {
+            return <div><ErrorMessage error={error} />{ button }</div>;
+          }
+          return <div className="create-book__button">
+            {button}
+          </div>; 
+        }}
+      </Mutation>
+    </div>
+  </div>
+);
 }
 
 export default CreateBook
