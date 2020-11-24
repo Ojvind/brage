db7e2c7724953fc01cb9173cd08c889ff0affc9c
Öjvind Otterbjörk <ojvind@otterbjork.com>
2020-11-24 16:54:24 +0100

    Remove Loading component for now
    ...fix that later! :-)

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
-
 import './style.css';
 
-const TICK_RATE = 5000;
+// const TICK_RATE = 5000;
 
 class Loading extends Component {
-  state = {
-    dots: 0,
-  };
+  // state = {
+  //   dots: 0,
+  // };
 
-  componentDidMount() {
-    this.interval = setInterval(this.onTick, TICK_RATE);
-  }
+  // componentDidMount() {
+  //   this.interval = setInterval(this.onTick, TICK_RATE);
+  // }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
 
-  onTick = () => {
-    this.setState(prevState => ({ dots: (prevState.dots + 1) % 4 }));
-  };
+  // onTick = () => {
+  //   this.setState(prevState => ({ dots: (prevState.dots + 1) % 4 }));
+  // };
 
   render() {
-    const { isCenter } = this.props;
-    const { dots } = this.state;
+  //   const { isCenter } = this.props;
+  //   const { dots } = this.state;
 
-    const classNames = ['Loading'];
+  //   const classNames = ['Loading'];
 
-    if (isCenter) {
-      classNames.push('Loading_center');
-    }
+  //   if (isCenter) {
+  //     classNames.push('Loading_center');
+  //   }
 
     return (
-      <div className={classNames.join(' ')}>
+      <div>
+      {/* <div className={classNames.join(' ')}> */}
         <small>
-          Loading {new Array(dots).fill(0).map(dot => '.')}
+          {/* Loading {new Array(dots).fill(0).map(dot => '.')} */}
         </small>
       </div>
     );
