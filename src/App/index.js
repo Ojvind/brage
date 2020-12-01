0a1a58c2f51b53268efe590c4ce778f71cde8b04
Öjvind Otterbjörk <ojvind@otterbjork.com>
2020-12-01 16:05:32 +0100

    Removed old code
    About, Home, NoMatch and TopicList was a rest
    from a old test.

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { Home } from './views/Home';
-import { About } from './views/About';
-import { TopicList } from './views/TopicList';
-import { NoMatch } from './views/NoMatch';
-import { TopicDetail } from './components/TopicDetail';
-
 import Navigation from './Navigation';
 import Footer from './Footer';
 
@@ -26,14 +20,9 @@ class App extends Component {
           />
           <div className="App-main">
             <Switch>
-              <Route exact path="/Home" component={Home} />
-              <Route exact path="/About" component={About} />
-              <Route exact path="/Topics" component={TopicList} />
-              <Route path="/Topics/:topicId" component={TopicDetail} />
               <Route exact path="/writers" component={Writer} />
               <Route path="/writers/:id/:name/:surname" component={WriterListItemDetailContainer} />
               <Route path="/books/:id/:title" component={BookListItemDetail} />
-              <Route component={NoMatch} />
             </Switch>
           </div>
           <Footer />
--- src/App/views/Home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = (props) => (
-  <div>
-    <h3>Home View</h3>
-  </div>
-);
-
-export default Home;
